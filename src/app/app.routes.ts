245ad5fa43400dff9156e9960c9a343c5e7ef341
Laplec2634 <Sebastianrojasmolinagm@gmail.com>
2025-11-07 19:13:43 -0500

    Guardado archivos de primer cambio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'inicio',
+    pathMatch: 'full'
+  },
+  {
+    path: 'inicio',
+    loadComponent: () => import('./pages/inicio/inicio').then(m => m.InicioComponent)
+  },
+  {
+    path: 'menu',
+    loadComponent: () => import('./pages/menu/menu').then(m => m.MenuComponent)
+  },
+  {
+    path: 'reserva',
+    loadComponent: () => import('./pages/reserva/reserva').then(m => m.ReservaComponent)
+  },
+  {
+    path: 'sobre-nosotros',
+    loadComponent: () => import('./pages/sobre-nosotros/sobre-nosotros').then(m => m.SobreNosotrosComponent)
+  },
+  {
+    path: 'contacto',
+    loadComponent: () => import('./pages/contacto/contacto').then(m => m.ContactoComponent)
+  },
+  {
+    path: '**',
+    redirectTo: 'inicio'
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header';
+import { ScrollToTopComponent } from './components/scroll-to-top/scroll-to-top';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    ScrollToTopComponent
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
-export class App {
-  protected readonly title = signal('restaurante-app');
+export class AppComponent {
+  title = 'restaurante-app';
 }
